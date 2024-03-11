08ebded84c0ff4e9b438e7138c65677709d83a24
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-11 16:46:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+//your code hereconst image = document.querySelectorAll(".image");
+
+image.forEach((image)=>{
+image.addEventListener("dragstart",dragStart)
+image.addEventListener("dragover",dragOver)
+image.addEventListener("drop",drop)
+})
+
+function dragStart(e){
+    e.preventDefault;
+    e.dataTransfer.setData("text/plain",e.target.id)
+
+}
+function dragOver(e){
+    e.preventDefault();
+}
+function drop(e){
+    e.preventDefault();
+    const draggedId = e.dataTransfer.getData("text/plain");
+    const draggedElement = document.getElementById(draggedId);
+    const dropzone = this;
+
+    if(dropzone.nextSibling == draggedElement){
+        dropzone.parentNode.insertBefore(draggedElement, dropzone)
+    }
+    else{
+        dropzone.parentNode.insertBefore(draggedElement, dropzone.nextSibling);
+    }
+}
